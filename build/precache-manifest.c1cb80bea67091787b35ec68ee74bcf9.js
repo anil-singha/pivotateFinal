9d29c29f9372a443d1de84fe793e673865ba2438
YizYah <yyakovson613@gmail.com>
2020-02-10 11:23:55 +0200

    added build directory

---
+++ build/precache-manifest.c1cb80bea67091787b35ec68ee74bcf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4053ee938df1c9cca2bb3d690019402",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66c4f88e9ad7c2dbdf20",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4e95bc85d0106c229226",
+    "url": "/static/js/2.2514fd5d.chunk.js"
+  },
+  {
+    "revision": "549ffe7e89f71f6998156972087b9d3b",
+    "url": "/static/js/2.2514fd5d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "66c4f88e9ad7c2dbdf20",
+    "url": "/static/js/main.1f3bfbcb.chunk.js"
+  },
+  {
+    "revision": "78c892ebc939d9ab90af",
+    "url": "/static/js/runtime-main.c46cf1f0.js"
+  }
+]);
