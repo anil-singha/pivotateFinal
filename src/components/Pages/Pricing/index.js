248c6c17607b9a99347f0e91dcd608ec98ba8952
clark candia <clarkcandia@outlook.com>
2020-02-12 23:43:20 +0800

    Initial commit

---
+++ src/components/Pages/Pricing/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Pricing = () => {
+  return (
+    <div className="container" style={{ height: "50vh" }}>
+      <h3>Pricing </h3>
+      <br></br>
+    </div>
+  );
+};
+
+export default Pricing;
