b68204d6a6112404fbe73fc066a829aef7c8879a
clark candia <clarkcandia@outlook.com>
2020-02-15 14:43:48 +0800

    Feat: Change heading size

--- src/components/Pages/Privacy-policy/index.js
+++ src/components/Pages/Privacy-policy/index.js
@@ -4,7 +4,7 @@ const UnderConstruction = () => {
   return (
     <div class="container full-width-sm">
       <div class="row">
-        <h1>PRIVACY POLICY</h1>
+        <h3>PRIVACY POLICY</h3>
         <p style={{ margin: "2em 0" }}>Effective: February 12, 2019</p>
         <p style={{ margin: "2em 0", textAlign: "right" }}>
           <a href="javascript:window.cmpConfig.methods.summon()">
@@ -90,7 +90,12 @@ const UnderConstruction = () => {
             Additional Information Regarding Children’s Privacy
           </a>
         </div>
-        <h2 id="how-we-collect-information">How We Collect Information</h2>
+        <h4
+          style={{ display: "block", fontWeight: "800", marginTop: "24px" }}
+          id="how-we-collect-information"
+        >
+          How We Collect Information
+        </h4>
         <p>
           Wiley may collect personal information about you in the following
           ways: (1) directly from your verbal or written input (such as by
@@ -104,7 +109,12 @@ const UnderConstruction = () => {
           behavioral data, or location data; or (3) by closed-circuit television
           (if visiting our premises).
         </p>
-        <h2 id="information-you-provide">Information You Provide</h2>
+        <h4
+          style={{ display: "block", fontWeight: "800", marginTop: "24px" }}
+          id="information-you-provide"
+        >
+          Information You Provide
+        </h4>
         <p>
           The types of personal information that Wiley collects directly from
           you or through your use of our websites and services may include:
@@ -153,9 +163,12 @@ const UnderConstruction = () => {
           you give permission for your Wiley account to be linked to your
           third-party account.
         </p>
-        <h2 id="information-we-receive">
+        <h4
+          style={{ display: "block", fontWeight: "800", marginTop: "24px" }}
+          id="information-we-receive"
+        >
           Information We Receive from Other Sources
-        </h2>
+        </h4>
         <p>
           Wiley may receive information about you if you use any of the websites
           we operate or the other services we provide. We also work closely with
@@ -166,7 +179,12 @@ const UnderConstruction = () => {
           search information providers, and credit reference agencies) from whom
           Wiley may receive information about you.
         </p>
-        <h2 id="use-of-your-information">Use of Your Information</h2>
+        <h4
+          style={{ display: "block", fontWeight: "800", marginTop: "24px" }}
+          id="use-of-your-information"
+        >
+          Use of Your Information
+        </h4>
         <p>
           Wiley may use your personal information in the performance of any
           contract we enter into with you, to comply with legal obligations, or
@@ -229,9 +247,12 @@ const UnderConstruction = () => {
           following a request from you to destroy or erase your personal
           information.
         </p>
-        <h2 id="disclosure-and-sharing">
+        <h4
+          style={{ display: "block", fontWeight: "800", marginTop: "24px" }}
+          id="disclosure-and-sharing"
+        >
           Disclosure and Sharing of Your Information
-        </h2>
+        </h4>
         <p>
           Wiley will not disclose to or share your personal information with any
           unaffiliated third party except as follows:
@@ -320,7 +341,12 @@ const UnderConstruction = () => {
           anonymous, aggregate usage statistics and demographic information that
           does not reveal your identity or personal information.
         </p>
-        <h2 id="cross-border-transfers">Cross Border Transfers</h2>
+        <h4
+          style={{ display: "block", fontWeight: "800", marginTop: "24px" }}
+          id="cross-border-transfers"
+        >
+          Cross Border Transfers
+        </h4>
         <p>
           Wiley may transfer your personal information outside of your country
           of residence for the following reasons:
@@ -350,7 +376,12 @@ const UnderConstruction = () => {
           treated securely and in accordance with this Privacy Policy and all
           applicable data protection laws.
         </p>
-        <h2 id="security">Security</h2>
+        <h4
+          style={{ display: "block", fontWeight: "800", marginTop: "24px" }}
+          id="security"
+        >
+          Security
+        </h4>
         <p>
           We will use appropriate physical, technical and administrative
           safeguards to protect your data. Access to your personal data will be
@@ -359,7 +390,12 @@ const UnderConstruction = () => {
           employees about the importance of maintaining the confidentiality and
           security of your information.
         </p>
-        <h2 id="chat-rooms-or-forums">Disclosure in Chat Rooms or Forums</h2>
+        <h4
+          style={{ display: "block", fontWeight: "800", marginTop: "24px" }}
+          id="chat-rooms-or-forums"
+        >
+          Disclosure in Chat Rooms or Forums
+        </h4>
         <p>
           You should be aware that identifiable personal information — such as
           your name or e-mail address — that you voluntarily disclose and that
@@ -368,7 +404,12 @@ const UnderConstruction = () => {
           Wiley cannot take any responsibility for such collection and
           disclosure.
         </p>
-        <h2 id="cookies">Cookies</h2>
+        <h4
+          style={{ display: "block", fontWeight: "800", marginTop: "24px" }}
+          id="cookies"
+        >
+          Cookies
+        </h4>
         <p>
           As is true of most websites, we gather certain information
           automatically. This information may include IP addresses, browser
@@ -392,7 +433,12 @@ const UnderConstruction = () => {
           </a>
           .
         </p>
-        <h2 id="your-rights">Your Rights</h2>
+        <h4
+          style={{ display: "block", fontWeight: "800", marginTop: "24px" }}
+          id="your-rights"
+        >
+          Your Rights
+        </h4>
         <p>
           You have the right to make a written request to be informed whether or
           not we hold or process any of your personal information (by emailing{" "}
@@ -452,7 +498,12 @@ const UnderConstruction = () => {
           </a>{" "}
           for the relevant contact details.
         </p>
-        <h2 id="third-parties">Third-Parties</h2>
+        <h4
+          style={{ display: "block", fontWeight: "800", marginTop: "24px" }}
+          id="third-parties"
+        >
+          Third-Parties
+        </h4>
         <p>
           Wiley’s websites or services may include links to third-party
           websites. In using such links, please be aware that each third-party
@@ -469,7 +520,12 @@ const UnderConstruction = () => {
           these and other websites in order to provide advertisements about
           goods and services likely to be of greater interest to you.
         </p>
-        <h2 id="ferpa">FERPA</h2>
+        <h4
+          style={{ display: "block", fontWeight: "800", marginTop: "24px" }}
+          id="ferpa"
+        >
+          FERPA
+        </h4>
         <p>
           Please see Wiley’s policy regarding FERPA{" "}
           <a href="https://www.wiley.com/ferpa" target="_blank">
@@ -477,7 +533,12 @@ const UnderConstruction = () => {
           </a>
           .
         </p>
-        <h2 id="recourse">Recourse</h2>
+        <h4
+          style={{ display: "block", fontWeight: "800", marginTop: "24px" }}
+          id="recourse"
+        >
+          Recourse
+        </h4>
         <p>
           Any comments, complaints or questions concerning this policy or
           complaints or objections about our use of your personal information
@@ -516,7 +577,12 @@ const UnderConstruction = () => {
           email address is{" "}
           <a href="mailto:casefiling@adr.org">casefiling@adr.org</a>.
         </p>
-        <h2 id="changes-to-privacy-policy">Updates to This Privacy Policy</h2>
+        <h4
+          style={{ display: "block", fontWeight: "800", marginTop: "24px" }}
+          id="changes-to-privacy-policy"
+        >
+          Updates to This Privacy Policy
+        </h4>
         <p>
           Please note that Wiley’s Privacy Policy is reviewed periodically.
           Wiley reserves the right to update its Privacy Policy at any time
@@ -550,9 +616,12 @@ const UnderConstruction = () => {
             </ul>
           </li>
         </ul>
-        <h2 id="children">
+        <h4
+          style={{ display: "block", fontWeight: "800", marginTop: "24px" }}
+          id="children"
+        >
           Additional Information Regarding Children’s Privacy
-        </h2>
+        </h4>
         <p>
           If we ask a child age 13 or under to provide us with certain
           information such as the child’s full name, home address, email
@@ -573,6 +642,8 @@ const UnderConstruction = () => {
           111 River Street, Hoboken, NJ 07030.
         </p>
       </div>
+      <br></br>
+      <br></br>
     </div>
   );
 };
