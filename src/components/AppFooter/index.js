2db45f2dac4c814742c73448184d51717de37a7e
clark candia <clarkcandia@outlook.com>
2020-02-15 13:46:10 +0800

    Feat: Add signup modal and redirection to under construction

--- src/components/AppFooter/index.js
+++ src/components/AppFooter/index.js
@@ -1,79 +1,117 @@
 import React from "react";
 
-const AppFooter = () => (
-  <footer class="footer">
-    <div class="footer__sign-up  ">
-      <div class="container triangle--white triangle">
-        <div class="flex">
-          <div class="grow-6">
+const AppFooter = props => (
+  <footer className="footer">
+    <div className="footer__sign-up  ">
+      <div className="container triangle--white triangle">
+        <div className="flex">
+          <div className="grow-6">
             <h2>
               <div>Don't settle with</div>
               <div>templates or freelancers!</div>
             </h2>
           </div>
-          <div class="grow-6 text-center">
-            <a href="/dialogs.html" class="button button--large button--teal">
-              SIGN UP <span class="chevron right"></span>
+          <div className="grow-6 text-center">
+            <a
+              onClick={props.onSignUp}
+              href="javascript:void(0);"
+              className="button button--large button--teal"
+            >
+              SIGN UP <span className="chevron right"></span>
             </a>
           </div>
         </div>
       </div>
     </div>
-    <div class="footer__content">
-      <div class="container">
-        <div class="flex">
-          <div class=" grow-6">
+    <div className="footer__content">
+      <div className="container">
+        <div className="flex">
+          <div className=" grow-6">
             <div>
               <img src="images/Pivotate Logo Invert.png" alt="Pivotate Logo" />
             </div>
             <div>
-              <ul class="flex external__links">
-                <li>FAQs</li>
-                <li>Blog</li>
-                <li>Contact</li>
+              <ul className="flex external__links">
+                <li>
+                  {" "}
+                  <a href="/under-construction"> FAQs</a>
+                </li>
+                <li>
+                  {" "}
+                  <a href="/under-construction"> Blog</a>
+                </li>
+                <li>
+                  {" "}
+                  <a href="/under-construction"> Contact</a>
+                </li>
               </ul>
             </div>
-            <div class="powered-by">
-              <span class="grey--text"> Powered By </span>
-              <a href="#" class="teal--text">
+            <div className="powered-by">
+              <span className="grey--text"> Powered By </span>
+              <a href="javascript:void(0);" className="teal--text">
                 {" "}
                 NoStack
               </a>
             </div>
           </div>
-          <div class="grow-6 ">
-            <div class="flex footer__group-links">
-              <div class="grow-4">
-                <div class="footer__title">Connect with us</div>
-                <ul class="footer__links">
-                  <li>Facebook</li>
-                  <li>LinkedIn</li>
+          <div className="grow-6 ">
+            <div className="flex footer__group-links">
+              <div className="grow-4">
+                <div className="footer__title">Connect with us</div>
+                <ul className="footer__links">
+                  <li>
+                    {" "}
+                    <a href="/under-construction"> Facebook </a>
+                  </li>
+                  <li>
+                    {" "}
+                    <a href="/under-construction"> LinkedIn </a>
+                  </li>
                 </ul>
               </div>
-              <div class="grow-4">
-                <div class="footer__title">About</div>
-                <ul class="footer__links">
-                  <li>About Us</li>
-                  <li>How it Works</li>
-                  <li>Pricing</li>
+              <div className="grow-4">
+                <div className="footer__title">About</div>
+                <ul className="footer__links">
                   <li>
-                    <a href="/dialogs.html" class="teal--text">
+                    {" "}
+                    <a href="/under-construction"> About Us</a>
+                  </li>
+                  <li>
+                    {" "}
+                    <a href="/under-construction"> How it Works</a>
+                  </li>
+                  <li>
+                    {" "}
+                    <a href="/under-construction"> Pricing</a>
+                  </li>
+                  <li>
+                    <a
+                      href="javascript:void(0);"
+                      onClick={props.onSignUp}
+                      className="teal--text"
+                    >
                       Sign Up Now
                     </a>
                   </li>
                 </ul>
               </div>
-              <div class="grow-4">
-                <div class="footer__title">Terms</div>
-                <ul class="footer__links">
-                  <li>Terms of Use</li>
-                  <li>Privacy</li>
+              <div className="grow-4">
+                <div className="footer__title">Terms</div>
+                <ul className="footer__links">
+                  <li>
+                    {" "}
+                    <a href="/terms-and-condition"> Terms of Use</a>
+                  </li>
+                  <li>
+                    {" "}
+                    <a href="/privacy-policy"> Privacy</a>
+                  </li>
                 </ul>
               </div>
             </div>
           </div>
         </div>
-        <div class="text-center copyright">Copyright © 2020 Pivotate</div>
+        <div className="text-center copyright">Copyright © 2020 Pivotate</div>
       </div>
     </div>
   </footer>
