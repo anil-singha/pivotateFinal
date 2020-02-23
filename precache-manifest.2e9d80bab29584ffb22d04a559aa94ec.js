3cb76d52ccd062cf2d30de5e6049cc6b69c761db
clark candia <clarkcandia@outlook.com>
2020-02-23 23:36:16 +0800

    Updates

---
+++ precache-manifest.2e9d80bab29584ffb22d04a559aa94ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b72e2f0b4926288f93465ff465583c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b413216d3cd3b764759f",
+    "url": "/static/css/main.cc2d9456.chunk.css"
+  },
+  {
+    "revision": "c88bd61908be2ac0afb9",
+    "url": "/static/js/2.24b74767.chunk.js"
+  },
+  {
+    "revision": "7da847b4390e9e97774ce02590b5ebae",
+    "url": "/static/js/2.24b74767.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b413216d3cd3b764759f",
+    "url": "/static/js/main.435340c0.chunk.js"
+  },
+  {
+    "revision": "78c892ebc939d9ab90af",
+    "url": "/static/js/runtime-main.c46cf1f0.js"
+  },
+  {
+    "revision": "d3203b9b30d3114b70777b56a68fbbf6",
+    "url": "/static/media/How It Works Mobile.d3203b9b.png"
+  },
+  {
+    "revision": "b14bb50597153e9b30cc21d826499358",
+    "url": "/static/media/How It Works.b14bb505.png"
+  }
+]);
