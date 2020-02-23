84af02a5b4295f229c369df648af203fa84d2914
clark candia <clarkcandia@outlook.com>
2020-02-23 20:00:21 +0800

    Feat: Add breakline

--- src/components/Pages/Pricing/index.js
+++ src/components/Pages/Pricing/index.js
@@ -3,6 +3,7 @@ import React from "react";
 const Pricing = () => {
   return (
     <div className="container" style={{ height: "50vh" }}>
+      <br></br>
       <div id="placeholders">
         <h3>Pricing </h3>
         <br></br>
