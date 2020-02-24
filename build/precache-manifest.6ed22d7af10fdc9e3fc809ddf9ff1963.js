ba82fca09c26b43e188cb0047581e54f24b043c8
Clark Candia <clarkcandia@outlook.com>
2020-02-24 12:03:36 +0800

    Fix not showing close button

---
+++ build/precache-manifest.6ed22d7af10fdc9e3fc809ddf9ff1963.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1febfded16d4b99d8af45a5c38442d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "052d6d146f133d9c30a6",
+    "url": "/static/css/main.01174f9c.chunk.css"
+  },
+  {
+    "revision": "94db7250130490a32d3c",
+    "url": "/static/js/2.9f9680f6.chunk.js"
+  },
+  {
+    "revision": "7da847b4390e9e97774ce02590b5ebae",
+    "url": "/static/js/2.9f9680f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "052d6d146f133d9c30a6",
+    "url": "/static/js/main.a1d94ca8.chunk.js"
+  },
+  {
+    "revision": "78c892ebc939d9ab90af",
+    "url": "/static/js/runtime-main.c46cf1f0.js"
+  },
+  {
+    "revision": "d3203b9b30d3114b70777b56a68fbbf6",
+    "url": "/static/media/How It Works Mobile.d3203b9b.png"
+  },
+  {
+    "revision": "b14bb50597153e9b30cc21d826499358",
+    "url": "/static/media/How It Works.b14bb505.png"
+  }
+]);
