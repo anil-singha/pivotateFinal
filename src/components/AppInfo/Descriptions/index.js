cd51ef15d3bf236ba07f579d43d6bc2dbf44212f
clark candia <clarkcandia@outlook.com>
2020-03-03 21:33:59 +0800

    Changes

--- src/components/AppInfo/Descriptions/index.js
+++ src/components/AppInfo/Descriptions/index.js
@@ -1,9 +1,9 @@
-import React, { Component, createRef } from 'react';
-import styled from 'styled-components';
-import { v4 } from 'uuid';
+import React, { Component, createRef } from "react";
+import styled from "styled-components";
+import { v4 } from "uuid";
 
-import DescriptionCreationForm from '../DescriptionCreationForm';
-import Description from '../Description';
+import DescriptionCreationForm from "../DescriptionCreationForm";
+import Description from "../Description";
 
 const DescriptionsStyleWrapper = styled.div``;
 
@@ -14,57 +14,56 @@ const DescriptionsStyleWrapper = styled.div``;
 
 class Descriptions extends Component {
   state = {
-    selectedDescriptionId: null,
+    selectedDescriptionId: null
   };
 
   wrapperRef = createRef();
 
   componentDidMount() {
-    document.addEventListener('mousedown', this.handleClick);
+    document.addEventListener("mousedown", this.handleClick);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClick);
+    document.removeEventListener("mousedown", this.handleClick);
   }
 
   handleClick = e => {
     const node = this.wrapperRef.current;
 
-    if (
-      node &&
-      node !== e.target &&
-      !node.contains(e.target)
-    ) {
+    if (node && node !== e.target && !node.contains(e.target)) {
       this.setState({ selectedDescriptionId: null });
     }
-  }
+  };
 
   handleSelect = id => this.setState({ selectedDescriptionId: id });
 
-  render () {
+  render() {
     const { appId, descriptions, refetchQueries, onUpdate } = this.props;
     const { selectedDescriptionId } = this.state;
 
     return (
-      <DescriptionsStyleWrapper ref={this.wrapperRef} onClick={this.handleClick}>
+      <DescriptionsStyleWrapper
+        ref={this.wrapperRef}
+        onClick={this.handleClick}
+      >
         <DescriptionCreationForm
-          parentId={ appId }
+          parentId={appId}
           refetchQueries={refetchQueries}
         />
 
-        { descriptions.map(description => (
+        {descriptions.map(description => (
           <Description
             key={v4()}
-            description={ description }
-            selected={ description.id === selectedDescriptionId }
+            description={description}
+            selected={description.id === selectedDescriptionId}
             onUpdate={onUpdate}
-            parentId={ appId }
+            parentId={appId}
             refetchQueries={refetchQueries}
             onSelect={this.handleSelect}
           />
-        )) }
+        ))}
       </DescriptionsStyleWrapper>
-    )
+    );
   }
 }
 
