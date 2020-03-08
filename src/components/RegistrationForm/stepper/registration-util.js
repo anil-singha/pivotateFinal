557cefbf3e7471a4e425317738510431106649f9
YizYah <yyakovson613@gmail.com>
2020-03-08 18:18:45 +0200

    added proper validation rule that password must contain a special character

--- src/components/RegistrationForm/stepper/registration-util.js
+++ src/components/RegistrationForm/stepper/registration-util.js
@@ -20,7 +20,8 @@ export const validationSchemaBasic = Yup.object().shape({
     .label('password')
     .matches(/[a-z]/, 'at least one lowercase char')
     .matches(/[A-Z]/, 'at least one uppercase char')
-    .matches(/[a-zA-Z]+[^a-zA-Z\s]+/, 'at least 1 number or special char (@,!,#, etc).')
+    .matches(/[0-9]/, 'at least 1 number')
+    .matches(/[^a-zA-Z0-9\s]/, 'at least 1 special char (@,!,#, etc).')
     .min(8, 'Must be at least 8 characters.')
     .required('Please enter your desired password.'),
   passwordConfirmation: Yup.string()
