6e154326702f9a3b2e763aca250e9c7fcd0d0db5
clark candia <clarkcandia@outlook.com>
2020-03-10 02:27:04 +0800

    Updates

---
+++ precache-manifest.d7fbd5503cb524b0dca2cd917af8f12c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001b73c765041ac075a949499658725b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d77760fbab8f04f2064",
+    "url": "/static/css/main.e3fb9bb3.chunk.css"
+  },
+  {
+    "revision": "6db32ca0c0c512801c28",
+    "url": "/static/js/10.7b1dfc6d.chunk.js"
+  },
+  {
+    "revision": "478e6a99d51cb2911682",
+    "url": "/static/js/2.0fcb10fd.chunk.js"
+  },
+  {
+    "revision": "7da847b4390e9e97774ce02590b5ebae",
+    "url": "/static/js/2.0fcb10fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2083e078ec5dbc619a58",
+    "url": "/static/js/3.04847e3a.chunk.js"
+  },
+  {
+    "revision": "06d28264830747c3d19d",
+    "url": "/static/js/4.4e642dd8.chunk.js"
+  },
+  {
+    "revision": "fbfc6468c27e09af6272",
+    "url": "/static/js/5.0a64ea58.chunk.js"
+  },
+  {
+    "revision": "9c88898a410c96c550fb",
+    "url": "/static/js/6.3891ca57.chunk.js"
+  },
+  {
+    "revision": "3b015cec4e96b325b41a",
+    "url": "/static/js/7.7ebd8bc2.chunk.js"
+  },
+  {
+    "revision": "b78c7dd7a10bade7dba3",
+    "url": "/static/js/8.55ce2a91.chunk.js"
+  },
+  {
+    "revision": "caaf9a6654bec3631a14",
+    "url": "/static/js/9.feeb43a8.chunk.js"
+  },
+  {
+    "revision": "7d77760fbab8f04f2064",
+    "url": "/static/js/main.1d125388.chunk.js"
+  },
+  {
+    "revision": "5745f179a475dc5797c6",
+    "url": "/static/js/runtime-main.11b84e2b.js"
+  },
+  {
+    "revision": "c258319b06dd0b6177a0df991d1c6171",
+    "url": "/static/media/How It Works Mobile min.c258319b.png"
+  },
+  {
+    "revision": "faa7c01316d395888d1e9022438f1626",
+    "url": "/static/media/How It Works min.faa7c013.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.850da2072997e83b899617ebdd12c541.js"
+  "/precache-manifest.d7fbd5503cb524b0dca2cd917af8f12c.js"
 );
 
 self.addEventListener('message', (event) => {
