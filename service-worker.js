728711fb654bb0e3a7490bbcdb5d8ef75bc010ae
clark candia <clarkcandia@outlook.com>
2020-03-10 02:07:10 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.69887036b9ce1c2cff531cbcc0d4c783.js"
+  "/precache-manifest.850da2072997e83b899617ebdd12c541.js"
 );
 
 self.addEventListener('message', (event) => {
--- static/js/6.70dfa789.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpno-stack-base"]=this["webpackJsonpno-stack-base"]||[]).push([[6],{595:function(s,a){}}]);
-//# sourceMappingURL=6.70dfa789.chunk.js.map
