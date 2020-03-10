05ef55b7323b46d69400241891c215df7c9bdaa1
Clark Candia <clarkcandia@outlook.com>
2020-03-10 12:47:35 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f4d1b8d1d04fa0654ba52da3fe73d099.js"
+  "/precache-manifest.b88c68b207d74cebb1116f7a4b002444.js"
 );
 
 self.addEventListener('message', (event) => {
