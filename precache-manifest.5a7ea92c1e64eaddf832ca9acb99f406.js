97666889a2ea058cde6354486ed61b580a56b136
Gynnaux <rso.janc@gmail.com>
2020-08-07 15:23:59 +0800

    Updates

---
+++ precache-manifest.5a7ea92c1e64eaddf832ca9acb99f406.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a5a2ef78739f8e2670544be699f0a24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e522043bd50a9635448f",
+    "url": "/static/css/16.389798f9.chunk.css"
+  },
+  {
+    "revision": "d50f25d77f76d850e690",
+    "url": "/static/css/main.0056316d.chunk.css"
+  },
+  {
+    "revision": "a3e82a2eba48a76e380c",
+    "url": "/static/js/0.2ebcfc6a.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/static/js/0.2ebcfc6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ebf8980b6622bb2829a1",
+    "url": "/static/js/1.7301edf4.chunk.js"
+  },
+  {
+    "revision": "5bbc1cd1f0ecfa58f5bb",
+    "url": "/static/js/10.08bd5705.chunk.js"
+  },
+  {
+    "revision": "a00c433b59e367343445",
+    "url": "/static/js/11.8c74f1fd.chunk.js"
+  },
+  {
+    "revision": "5a054fe79b3d4d3e6b04",
+    "url": "/static/js/12.b1f7f0d4.chunk.js"
+  },
+  {
+    "revision": "3f86d80cda1c9ef978b3",
+    "url": "/static/js/13.dbb03376.chunk.js"
+  },
+  {
+    "revision": "5ed030e7b435e48c289e",
+    "url": "/static/js/14.8b3db6e2.chunk.js"
+  },
+  {
+    "revision": "e54031535c1f158bf26b",
+    "url": "/static/js/15.b69c53e7.chunk.js"
+  },
+  {
+    "revision": "e522043bd50a9635448f",
+    "url": "/static/js/16.40165772.chunk.js"
+  },
+  {
+    "revision": "0560eea3300b80103d03",
+    "url": "/static/js/17.cf24928f.chunk.js"
+  },
+  {
+    "revision": "322a465b994e953b3bb0",
+    "url": "/static/js/18.3b256d02.chunk.js"
+  },
+  {
+    "revision": "9adab047dca7a49b28e6",
+    "url": "/static/js/19.3212bb5d.chunk.js"
+  },
+  {
+    "revision": "f5b76199f001e447184c",
+    "url": "/static/js/2.7e1b8428.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/static/js/2.7e1b8428.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54727fcdf252669d60ff",
+    "url": "/static/js/20.6d1ae237.chunk.js"
+  },
+  {
+    "revision": "46c5305a3902ad035a89",
+    "url": "/static/js/21.34b9682e.chunk.js"
+  },
+  {
+    "revision": "7bb24abbfdb639d4ed64",
+    "url": "/static/js/22.b2ab4faf.chunk.js"
+  },
+  {
+    "revision": "f04938b4d57874a6c91d",
+    "url": "/static/js/23.18cc155c.chunk.js"
+  },
+  {
+    "revision": "411bbde8aeece2fa8a4e98523dc3f4e3",
+    "url": "/static/js/23.18cc155c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "081770c1638d390d605c",
+    "url": "/static/js/3.d2913546.chunk.js"
+  },
+  {
+    "revision": "16cf377bcc7648a3eb07bd7d79f8ae96",
+    "url": "/static/js/3.d2913546.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9ff1f1908620582bfdb",
+    "url": "/static/js/4.06214251.chunk.js"
+  },
+  {
+    "revision": "ef22cf10a4c90e0c7b28",
+    "url": "/static/js/7.77b1be8d.chunk.js"
+  },
+  {
+    "revision": "2a47d7fd24195bb32fce3ad35ab697a1",
+    "url": "/static/js/7.77b1be8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b4232adbfdf50f5f47f5",
+    "url": "/static/js/8.77fd0078.chunk.js"
+  },
+  {
+    "revision": "411bbde8aeece2fa8a4e98523dc3f4e3",
+    "url": "/static/js/8.77fd0078.chunk.js.LICENSE"
+  },
+  {
+    "revision": "32927f3a5a883843c25c",
+    "url": "/static/js/9.b7ed9313.chunk.js"
+  },
+  {
+    "revision": "d50f25d77f76d850e690",
+    "url": "/static/js/main.142af8cc.chunk.js"
+  },
+  {
+    "revision": "ab3f471f6d9d70d1756d",
+    "url": "/static/js/runtime-main.6da0fa6d.js"
+  },
+  {
+    "revision": "7fb1344a0f6f51303b560d8e13fc5ee7",
+    "url": "/static/media/Banner.7fb1344a.jpg"
+  },
+  {
+    "revision": "c258319b06dd0b6177a0df991d1c6171",
+    "url": "/static/media/How It Works Mobile min.c258319b.png"
+  },
+  {
+    "revision": "faa7c01316d395888d1e9022438f1626",
+    "url": "/static/media/How It Works min.faa7c013.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6a132f4f13c38c208f5e2baca8069f7a.js"
+  "/precache-manifest.5a7ea92c1e64eaddf832ca9acb99f406.js"
 );
 
 self.addEventListener('message', (event) => {
